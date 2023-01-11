4b55fcee2511e9b9b4eec8ec343273e9531e5698
RadheyaM <radheyamansel@hotmail.com>
2023-01-11 15:45:09 +0000

    Add a home app and successfully display a home view by configuring view, url etc

--- core/settings.py
+++ core/settings.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    # my apps
+    'home',
 ]
 
 SITE_ID = 1
@@ -124,8 +126,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
