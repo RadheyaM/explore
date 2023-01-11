be79e9385078840760bc9b5108828b20e6b03046
RadheyaM <radheyamansel@hotmail.com>
2023-01-11 17:21:53 +0000

    Register the new models with admin site, upload first 30 books data along with category and genre data from fixtures

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Genre, Books
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Genre)
+admin.site.register(Books)
+
