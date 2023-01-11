ad9bf1944569a7684aa4384d6f2244f99a74181b
RadheyaM <radheyamansel@hotmail.com>
2023-01-11 23:58:32 +0000

    Add a book detail template view, populate links on the books page

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.books, name='books')
+    path('books/', views.books, name='books'),
+    path('books/<book_id>', views.book_detail, name='book_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Books
 
 
@@ -13,3 +13,16 @@ def books(request):
     }
 
     return render(request, 'products/books.html', context)
+
+
+def book_detail(request, book_id):
+    """
+    A view to show all book products available for sale.
+    """
+    book = get_object_or_404(Books, pk=book_id)
+
+    context = {
+        'book': book,
+    }
+
+    return render(request, 'products/book-detail.html', context)
