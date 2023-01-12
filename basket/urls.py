3354c6a00303688d29b2e2b114ec51b4b9787c55
RadheyaM <radheyamansel@hotmail.com>
2023-01-12 11:56:49 +0000

    Create an 'add to bag' view with logic creating a session dictionary to hold items user adds to the basket

--- basket/urls.py
+++ basket/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.basket, name='basket')
+    path('', views.basket, name='basket'),
+    path('add/<item_id>', views.add_to_basket, name='add_to_basket'),
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+"""Code based upon that of the walkthrough project boutique ado"""
+
+from django.shortcuts import render, redirect
 
 
 def basket(request):
@@ -7,3 +9,21 @@ def basket(request):
     """
     return render(request, 'basket/basket.html')
 
+
+def add_to_basket(request, item_id):
+    """
+    Add a qty of a particular item to the shopping basket
+    """
+
+    quantity =  int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
+
+    if item_id in list(basket.keys()):
+        basket[item_id] += quantity
+    else:
+        basket[item_id] = quantity
+
+    request.session['basket'] = basket
+    print(request.session['basket'])
+    return redirect(redirect_url)
