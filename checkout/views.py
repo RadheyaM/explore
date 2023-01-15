e285e157be73706b9782db37dd6a5589985fcca0
RadheyaM <radheyamansel@hotmail.com>
2023-01-15 10:08:16 +0000

    Add stripe card element to checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -8,14 +8,16 @@ def checkout(request):
     Render a view for the checkout
     """
     basket = request.session.get('basket', {})
-    if not basket:
-        messages.error(request, "There's nothing in your bag, better keep on shopping there buddy.")
-        return redirect(reverse('books'))
+    # if not basket:
+    #     message.error(request, "There's nothing in your bag, better keep on shopping there buddy.")
+    #     return redirect(reverse('books'))
     
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form: order_form,'
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MOja9GRqR0b7FUtJwlJHqFhnN5WhXXcSMx7cyUwZWyMzgNqanRT858TaJagYRjuM9TfdMZcs9xego30AgediDjP00xomNU3pn',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
