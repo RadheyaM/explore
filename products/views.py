cc03596b65626256fd40d38752e56528ead3c733
RadheyaM <radheyamansel@hotmail.com>
2023-01-16 11:19:04 +0000

    Add basic sorting to the books page

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Books
+from .models import Books, Genre
 
 
 def books(request):
@@ -7,9 +7,19 @@ def books(request):
     A view to show all book products available for sale.
     """
     books = Books.objects.all()
+    genres = None
+    all_genres = Genre.objects.all()
+
+    if request.GET:
+        if 'genre' in request.GET:
+            genres = request.GET['genre'].split(',')
+            books = books.filter(genre__name__in=genres)
+            genres = Genre.objects.filter(name__in=genres)
 
     context = {
         'books': books,
+        'genres': genres,
+        'all_genres': all_genres,
     }
 
     return render(request, 'products/books.html', context)
