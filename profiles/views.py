6bcaf39005fef002ac76b3a4b22a9ba0b2baa586
RadheyaM <radheyamansel@hotmail.com>
2023-01-16 20:11:08 +0000

    Improve the styling on the main allauth templates

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 
 def profile(request):
     """
-    A view to show the store homepage
+    A view to show the user profile page
     """
     return render(request, 'profile/profile.html')
