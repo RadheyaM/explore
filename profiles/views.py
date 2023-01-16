55bd1f06c339fb01d79161f86dc07a201c96d3ba
RadheyaM <radheyamansel@hotmail.com>
2023-01-16 20:44:23 +0000

    Make sure that profiles are created for exiting user, test a newaccountt and profile creation, add context to profile view displaying username

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
 
 
 def profile(request):
     """
     A view to show the user profile page
     """
-    return render(request, 'profile/profile.html')
+    profile = get_object_or_404(UserProfile, user=request.user)
+    context = {
+        'profile': profile,
+    }
+
+    return render(request, 'profile/profile.html', context)
