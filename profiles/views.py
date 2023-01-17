685011a102ff6e3d4d1a23f43135a8f2f952356f
RadheyaM <radheyamansel@hotmail.com>
2023-01-17 15:51:50 +0000

    Added a profile form to set profile default delivery address, view logic added and template improved

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
@@ -7,8 +10,18 @@ def profile(request):
     A view to show the user profile page
     """
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == "POST":
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'You successfully updated your profile!')
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
     }
 
     return render(request, 'profile/profile.html', context)
