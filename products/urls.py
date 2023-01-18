2910621d324b337f861529b5b6d81dbc4c5c4a34
RadheyaM <radheyamansel@hotmail.com>
2023-01-18 19:00:32 +0000

    Add poster products and product detail template, urls and view logic

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('books/', views.books, name='books'),
     path('books/<book_id>', views.book_detail, name='book_detail'),
+    path('posters/', views.posters, name='posters'),
+    path('posters/<poster_id>', views.poster_detail, name='poster_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Books, Genre
+from .models import Books, Genre, Posters
 
 
 def books(request):
@@ -16,13 +16,37 @@ def books(request):
             books = books.filter(genre__name__in=genres)
             genres = Genre.objects.filter(name__in=genres)
 
+    template = 'products/books.html'
     context = {
         'books': books,
         'genres': genres,
         'all_genres': all_genres,
     }
 
-    return render(request, 'products/books.html', context)
+    return render(request, template, context)
+
+def posters(request):
+    """
+    A view to show all poster products available for sale.
+    """
+    posters = Posters.objects.all()
+    genres = None
+    all_genres = Genre.objects.all()
+
+    if request.GET:
+        if 'genre' in request.GET:
+            genres = request.GET['genre'].split(',')
+            posters = posters.filter(genre__name__in=genres)
+            genres = Genre.objects.filter(name__in=genres)
+
+    template = 'products/posters.html'
+    context = {
+        'posters': posters,
+        'genres': genres,
+        'all_genres': all_genres,
+    }
+
+    return render(request, template, context)
 
 
 def book_detail(request, book_id):
@@ -34,9 +58,28 @@ def book_detail(request, book_id):
     genres = Books.objects.filter(genre=genre)
     recommend = genres.exclude(pk=book_id)
 
+    template = 'products/book-detail.html'
     context = {
         'book': book,
         'recommend': recommend,
     }
 
-    return render(request, 'products/book-detail.html', context)
+    return render(request, template, context)
+
+
+def poster_detail(request, poster_id):
+    """
+    A view to show all book products available for sale.
+    """
+    poster = get_object_or_404(Posters, pk=poster_id)
+    genre = poster.genre
+    genres = Posters.objects.filter(genre=genre)
+    recommend = genres.exclude(pk=poster_id)
+
+    template = 'products/poster-detail.html'
+    context = {
+        'poster': poster,
+        'recommend': recommend,
+    }
+
+    return render(request, template, context)
