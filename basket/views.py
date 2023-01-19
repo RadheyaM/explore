85ed0b3998b9aa4b955adea05478880387f93feb
RadheyaM <radheyamansel@hotmail.com>
2023-01-19 23:57:27 +0000

    Add includes templates to the base template with logic to display certain types of message alerts should messages exist
    Add some custom css styling

--- basket/views.py
+++ basket/views.py
@@ -1,7 +1,7 @@
 """Code based upon that of the walkthrough project boutique ado"""
-
 from django.shortcuts import render, redirect
-
+from django.contrib import messages
+from products.models import Books, Posters
 
 def basket(request):
     """
@@ -14,6 +14,10 @@ def add_to_basket(request, item_id):
     """
     Add a qty of a particular item to the shopping basket
     """
+    try:
+        product = Books.objects.get(code=item_id)
+    except:
+        product = Posters.objects.get(code=item_id)
 
     quantity =  int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -26,15 +30,22 @@ def add_to_basket(request, item_id):
         if item_id in list(basket.keys()):
             if material_size in basket[item_id]['items_by_material_size'].keys():
                 basket[item_id]['items_by_material_size'][material_size] += quantity
+                messages.success(request, f'Success! You added {product} to your basket!')
             else:
                 basket[item_id]['items_by_material_size'][material_size] = quantity
+                messages.error(request, f'Success! You added {product} to your basket!')
         else:
             basket[item_id] = {'items_by_material_size': {material_size: quantity}}
+            messages.warning(request, f'Success! You added {product} to your basket!')
     else:
         if item_id in list(basket.keys()):
             basket[item_id] += quantity
+            messages.info(request, f'Success! You added {product} to your basket!')
         else:
             basket[item_id] = quantity
+            messages.success(request, f'Success! You added {product} to your basket!')
+    
+    
 
     request.session['basket'] = basket
     return redirect(redirect_url)
