d747bd40d9baccf7ec72edb74ed097e8be71ba1e
RadheyaM <radheyamansel@hotmail.com>
2023-01-20 20:04:50 +0000

    Add Create views, templates and urls for creating categories and genres

--- crud/urls.py
+++ crud/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('manage/', views.manage, name='manage_products'),
     path('create-book/', views.CreateBookView.as_view(), name='create_book'),
+    path('create-poster/', views.CreatePosterView.as_view(), name='create_poster'),
+    path('create-category/', views.CreateCategoryView.as_view(), name='create_category'),
+    path('create-genre/', views.CreateGenreView.as_view(), name='create_genre'),
 ]
--- crud/views.py
+++ crud/views.py
@@ -11,13 +11,17 @@ def manage(request):
     template = 'crud/manage.html'
     return render(request, template)
 
+#====================================================================================
+#                                   CREATE VIEWS
+#====================================================================================
+
 class CreateBookView(CreateView):
     """
     View for Admin to create a book product
     """
     model = Books
     fields = '__all__'
-    template_name = 'crud/create-book.html'
+    template_name = 'crud/create/create-book.html'
     
     success_url = reverse_lazy('manage_products')
 
@@ -27,6 +31,66 @@ class CreateBookView(CreateView):
         """
         messages.add_message(
             self.request, messages.SUCCESS, "You successfully added a new Book product"
+        )
+
+        return super().form_valid(form)
+
+class CreatePosterView(CreateView):
+    """
+    View for Admin to create a book product
+    """
+    model = Posters
+    fields = '__all__'
+    template_name = 'crud/create/create-poster.html'
+    
+    success_url = reverse_lazy('manage_products')
+
+    def form_valid(self, form):
+        """
+        Custom form_valid function adding a success message for display.
+        """
+        messages.add_message(
+            self.request, messages.SUCCESS, "You successfully added a new Poster product"
+        )
+
+        return super().form_valid(form)
+
+class CreateCategoryView(CreateView):
+    """
+    View for Admin to create a book product
+    """
+    model = Category
+    fields = '__all__'
+    template_name = 'crud/create/create-category.html'
+    
+    success_url = reverse_lazy('manage_products')
+
+    def form_valid(self, form):
+        """
+        Custom form_valid function adding a success message for display.
+        """
+        messages.add_message(
+            self.request, messages.SUCCESS, "You successfully added a new Category"
+        )
+
+        return super().form_valid(form)
+
+class CreateGenreView(CreateView):
+    """
+    View for Admin to create a book product
+    """
+    model = Genre
+    fields = '__all__'
+    template_name = 'crud/create/create-genre.html'
+    
+    success_url = reverse_lazy('manage_products')
+
+    def form_valid(self, form):
+        """
+        Custom form_valid function adding a success message for display.
+        """
+        messages.add_message(
+            self.request, messages.SUCCESS, "You successfully added a new Genre"
         )
 
         return super().form_valid(form)
