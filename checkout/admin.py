619407b4ebabf99d4928c3e72dae500897132466
RadheyaM <radheyamansel@hotmail.com>
2023-01-20 16:54:34 +0000

    Edit the readonly fields of the order inline admin so as to only have adjustable qty due to the nature of the book and poster products

--- checkout/admin.py
+++ checkout/admin.py
@@ -5,7 +5,7 @@ from .models import Order, OrderLineItem
 
 class OrderLineItemAdminInline(admin.TabularInline):
     model = OrderLineItem
-    readonly_fields = ('lineitem_total', )
+    readonly_fields = ('product', 'book', 'poster', 'material_size', 'lineitem_total', )
 
 
 class OrderAdmin(admin.ModelAdmin):
