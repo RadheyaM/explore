94ce1119d3c4a51f9a35d0351bd25c4b467969f9
RadheyaM <radheyamansel@hotmail.com>
2023-01-22 02:25:28 +0000

    Improve the position of the message alert, add javascript to remove message on mouseclick event

---
+++ static/js/base.js
@@ -0,0 +1,6 @@
+document.addEventListener("click", () => {
+    var messageContainer = document.getElementById("message")
+    if (messageContainer !== null) {
+        document.getElementById("message").style.display = 'none';
+    }
+});
