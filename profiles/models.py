a7fbfd8a32f1d07bbefcffadf1baabfd2b8811c8
RadheyaM <radheyamansel@hotmail.com>
2023-01-27 22:46:48 +0000

    make improvements to the structure and styling of the contact pages on small screens especially

--- profiles/models.py
+++ profiles/models.py
@@ -40,9 +40,9 @@ class Contact(models.Model):
     class Meta:
         verbose_name_plural = 'Contact'
 
-    email = models.EmailField(help_text='We will respond to this address.', verbose_name='Your Email Address')
-    reason = models.CharField(max_length=64, help_text='Why are you contacting us today?', verbose_name='Message Subject')
-    body = models.TextField(max_length=1064, help_text='Write your message here.', verbose_name='Message')
+    email = models.EmailField(verbose_name='Your Email Address')
+    reason = models.CharField(max_length=64, verbose_name='Message Subject')
+    body = models.TextField(max_length=1064, verbose_name='Message')
     admin_responded = models.BooleanField(default='False')
 
     def __str__(self):
@@ -56,10 +56,10 @@ class SiteReview(models.Model):
 
     RATING = ((1, 1), (2, 2), (3, 3), (4, 4), (5, 5))
 
-    name = models.CharField(max_length=50, help_text='This name will be displayed as the author on the published review.', verbose_name='Your Name')
-    title = models.CharField(max_length=64, help_text='What is the title of your review?', verbose_name='Review Title')
-    rating = models.IntegerField(choices=RATING, help_text='Your rating out of 5 stars', verbose_name="Rate Us")
-    body = models.TextField(max_length=1064, help_text='Write your review here', verbose_name='Your Review')
+    name = models.CharField(max_length=50, verbose_name='Your Name')
+    title = models.CharField(max_length=64, verbose_name='Review Title')
+    rating = models.IntegerField(choices=RATING, verbose_name="Rate Us")
+    body = models.TextField(max_length=1064, verbose_name='Your Review')
     admin_approved = models.BooleanField(default='False')
 
     def __str__(self):
