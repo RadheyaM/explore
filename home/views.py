f7034997a616fcdc0d9ca85f840cdf7449ff34c2
RadheyaM <radheyamansel@hotmail.com>
2023-01-27 17:31:46 +0000

    Add a special feature section to the home page and make it responsive to all screen sizes

--- home/views.py
+++ home/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from products.models import Books
 
 
 def index(request):
     """
     A view to show the store homepage
     """
-    return render(request, 'home/index.html')
+
+    feature = get_object_or_404(Books, code='BHM9-XWP5-B48C-EKPB')
+
+    return render(request, 'home/index.html', {'book': feature})
 
 def about_us(request):
     """
